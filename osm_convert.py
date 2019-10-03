ad08483fe2d610e10ec727fec01e939efc2ef6a9
XiyuZhai97 <zxy4970@gmail.com>
2019-10-02 22:38:37 -0700

    Complete osmTwizardZ

--- osm_convert.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-import sumolib
-netconvert = sumolib.checkBinary('netconvert', bindir)
-netconvertOpts = [netconvert]
-netconvertOpts += ['--osm-files'] + ['berlin.osm.xml']
-netconvertOpts += ['-o'] + ['berlin.net.xml']
-print(netconvertOpts)
